a013a739ca1be14af94af2d0703454af1df733f2
Faisal Al-Zaben <98561297+fzaben993@users.noreply.github.com>
2024-09-25 19:05:37 +0300

    Update activate-win-22.ps1

--- activate-win-22.ps1
+++ activate-win-22.ps1
@@ -1,6 +1,17 @@
-dism /online /set-edition:serverdatacenter /productkey:WX4NM-KYWYW-QJJR4-XV3QB-6VM33 /accepteula
+# Win Server 2022
+# serverdatacenter
 
+dism /online /set-edition:serverdatacenter /productkey:WX4NM-KYWYW-QJJR4-XV3QB-6VM33 /accepteula
 slmgr /ipk WX4NM-KYWYW-QJJR4-XV3QB-6VM33
+
+# OR
+
+# ServerStandard
+
+dism /online /set-edition:ServerStandard /productkey:VDYBN-27WPP-V4HQT-9VMD4-VMK7H /accepteula
+slmgr /ipk VDYBN-27WPP-V4HQT-9VMD4-VMK7H
+
+# Common
 slmgr /skms kms8.msguides.com
 slmgr /ato
 

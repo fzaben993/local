1fc67b5592b7fa0aab28a2020f34d3eb03410c5b
Faisal Al-Zaben <98561297+fzaben993@users.noreply.github.com>
2024-09-25 19:48:50 +0300

    Update profile.ps1

--- profile.ps1
+++ profile.ps1
@@ -11,18 +11,18 @@ if (-not (Test-Path $profilePath)) {
 $functionCode = @"
 function github {
     param (
-        [string]$scriptName
+        [string]`$scriptName
     )
 
-    $baseUrl = 'https://raw.githubusercontent.com/fzaben993/local/main'
-    $scriptUrl = "\$baseUrl/\$scriptName.ps1"
+    `$baseUrl = 'https://raw.githubusercontent.com/fzaben993/local/main'
+    `$scriptUrl = "`$baseUrl/\$scriptName.ps1"
 
-    if (-not [string]::IsNullOrEmpty(\$scriptName)) {
+    if (-not [string]::IsNullOrEmpty(`$scriptName)) {
         try {
-            \$script = (New-Object System.Net.WebClient).DownloadString(\$scriptUrl)
-            Invoke-Expression \$script
+            `$script = (New-Object System.Net.WebClient).DownloadString(`$scriptUrl)
+            Invoke-Expression `$script
         } catch {
-            Write-Host "Error downloading or executing script: \$_"
+            Write-Host "Error downloading or executing script: `$_"
         }
     } else {
         Write-Host "Please provide a valid script name."
@@ -37,4 +37,4 @@ if (-not (Select-String -Path $profilePath -Pattern 'function github')) {
 }
 
 # Inform the user to reload the profile
-Write-Host "GitHub function added to your PowerShell profile. Please restart PowerShell or run `. $profilePath` to use it. "
+Write-Host "GitHub function added to your PowerShell profile. Please restart PowerShell or run `. $profilePath` to use it."
